bd8d75c7cde66315306f69cf5b13c75fa93f2f77
Rogovoy-Serhiy <123328242+Rogovoy-Serhiy@users.noreply.github.com>
2023-09-28 10:46:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let heart = document.querySelector('.sup_item-img');
+console.dir(heart.style.left);
+
+// function a(){
+//     for(let i = heart.style, i < ){
+//     setInterval()
+//     }
+// }
